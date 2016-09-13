1e4bce344851df916dbe4a5d08e968cc604df2b7
py <zhaopy4721@outlook.com>
2016-09-13 12:16:43 +0800

    add video download

--- 755-scrape.py
+++ 755-scrape.py
@@ -9,32 +9,56 @@ namelist=name.readlines()
 for i in range(len(namelist)):
     namelist[i]=namelist[i].strip()
 
+if not os.path.exists("755-download"):
+    os.mkdir("755-download")
 
-num=0
-snum=0
+
+inum=0
+isnum=0
+vnum=0
+vsnum=0
 for nameId in namelist:
-    print('Downloading {}\'s picture now...'.format(nameId))
+    print('Downloading {}\'s pictures and movies now...'.format(nameId))
     Turl="https://api.7gogo.jp/web/v2/talks/{}/posts".format(nameId)
     html=urlopen(Turl).read().decode('utf-8')
-    if not os.path.exists("images/{}".format(nameId)):
-        os.mkdir("images/{}".format(nameId))
-
+    if not os.path.exists("755-download/{}".format(nameId)):
+        os.mkdir("755-download/{}".format(nameId))
+    if not os.path.exists("755-download/{}/videos".format(nameId)):
+        os.mkdir("755-download/{}/videos".format(nameId))
+    if not os.path.exists("755-download/{}/images".format(nameId)):
+        os.mkdir("755-download/{}/images".format(nameId))
     jsonHtml=json.loads(html)
-    snum=0
+    isnum=0
+    vsnum=0
     for dataList in jsonHtml.get('data'):
         for bodyList in dataList.get('post').get('body'):
+            #download images
             url=bodyList.get('image')
             if url:
                 info=url.split('/')
                 imgtype=info[-1].split('.')
-                if imgtype[-1]=='jpg':
+                if imgtype[-1]=='jpg':  #ignore png
                     for i in range(3):
                         del info[0]
                     link='-'
                     filename=link.join(info)                    
-                    urlretrieve(url,"images/{}/image-{}".format(nameId,filename))
-                    num=num+1
-                    snum=snum+1
+                    urlretrieve(url,"755-download/{}/images/{}".format(nameId,filename))
+                    inum=inum+1
+                    isnum=isnum+1
                     print(url)
-    print("Download {} pictures from {}".format(snum,nameId))
-print("Download {} pictures in all".format(num))
+            #download videos
+            url=bodyList.get('movieUrlHq')
+            if url:
+                info=url.split('/')
+                imgtype=info[-1].split('.')
+                if 1==1:
+                    for i in range(3):
+                        del info[0]
+                    link='-'
+                    filename=link.join(info)                    
+                    urlretrieve(url,"755-download/{}/videos/{}".format(nameId,filename))
+                    vnum=vnum+1
+                    vsnum=vsnum+1
+                    print(url)
+    print("Download {} pictures and {} videos from {}".format(isnum,vsnum,nameId))
+print("Download {} pictures and {} videos in all".format(inum,vnum))
