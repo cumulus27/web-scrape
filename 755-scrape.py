513b0be5923abd33c14ff28bd4f5c9551e6e29e8
py <zhaopy4721@outlook.com>
2016-09-13 20:25:00 +0800

    add time limmit

--- 755-scrape.py
+++ 755-scrape.py
@@ -3,9 +3,11 @@ from urllib.request import urlretrieve
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+import time
 
-name=open('name.txt')
-namelist=name.readlines()
+
+with open('name.txt') as name:
+    namelist=name.readlines()
 for i in range(len(namelist)):
     namelist[i]=namelist[i].strip()
 
@@ -17,6 +19,7 @@ inum=0
 isnum=0
 vnum=0
 vsnum=0
+now=time.time()
 for nameId in namelist:
     print('Downloading {}\'s pictures and movies now...'.format(nameId))
     Turl="https://api.7gogo.jp/web/v2/talks/{}/posts".format(nameId)
@@ -31,7 +34,11 @@ for nameId in namelist:
     isnum=0
     vsnum=0
     for dataList in jsonHtml.get('data'):
-        for bodyList in dataList.get('post').get('body'):
+        onePost=dataList.get('post')
+        postTime=onePost.get('time')
+#        if now-postTime>86400:
+#            break
+        for bodyList in onePost.get('body'):
             #download images
             url=bodyList.get('image')
             if url:
